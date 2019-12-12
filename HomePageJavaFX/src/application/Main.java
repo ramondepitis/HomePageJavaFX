3510c185d0e45776176625741ecea08e19bb5211
Mario Cubero <46523054+ramondepitis@users.noreply.github.com>
2019-12-12 08:51:04 +0100

    Add files via upload

---
+++ HomePageJavaFX/src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+
+        FXMLLoader loader =new FXMLLoader(getClass().getResource("HomePage.fxml"));
+        AnchorPane pane=loader.load();
+        Scene scene=new Scene(pane);
+        scene.getStylesheets().addAll(getClass().getResource("style.css").toExternalForm());
+        primaryStage.setTitle("Home Page");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
